add982f05fc4b095460cf4139652d2d79a0fdc26
simonirang1992 <simonirang92@hotmail.com>
2016-02-26 01:02:23 -0500

    TODO

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
+# Author: Simon Song
+# Server deployment script for user
 #!/bin/sh
 sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible
 sudo apt-get install wget
-sudo ansible-playbook deploy.yml
